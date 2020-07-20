31d6197e3ad2f4e68adffdb88d3d8b06edb1d737
Jinmou Li <jinmli@google.com>
2020-07-20 23:32:14 +0000

    use command line argument as open path

--- open.c
+++ open.c
@@ -9,8 +9,13 @@
 
 extern int errno;
 
-int main() {
-    int fd = open("../nomount/hello", O_RDONLY);
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        printf("Error: need path to be the first argument\n");
+        return 1;
+    }
+
+    int fd = open(argv[1], O_RDONLY);
     if(fd < 0) {
         printf("Error: open failed %d\n", fd);
         printf("Error: %s (errno=%d)\n", strerror(errno), errno);
